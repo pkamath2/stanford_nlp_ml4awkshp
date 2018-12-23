caff4eb894da94c5a47b4ec67c2bfe1da3ac9e2d
pkamath2 <git.pkamath2@gmail.com>
2018-12-23 16:55:08 +0530

    Rest of the code

---
+++ src/main/java/org/pk/opennlp/Controller.java
@@ -0,0 +1,36 @@
+package org.pk.opennlp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *   Author: purnimakamath
+ */
+@RestController
+@RequestMapping(path="/ff")
+@CrossOrigin(origins = "http://localhost:9999")
+public class Controller {
+
+	@Autowired
+	Test_Text_analyser analyser;
+
+	@Bean(initMethod = "init")
+	public Test_Text_analyser getAnalyser(){
+		return new Test_Text_analyser();
+	}
+
+	@GetMapping(path = "analyse")
+	public String analyse(@RequestParam(name="start") String text){
+
+		long sTime = System.currentTimeMillis();
+		String ret = analyser.findSentiment(text);
+		System.out.println((System.currentTimeMillis() - sTime)/1000);
+		return ret;
+
+	}
+}
---
+++ src/main/java/org/pk/opennlp/Test_Text_analyser.java
@@ -0,0 +1,59 @@
+package org.pk.opennlp;
+
+/**
+ *   Author: purnimakamath
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Properties;
+import org.ejml.simple.SimpleMatrix;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations.SentimentAnnotatedTree;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.util.CoreMap;
+
+public class Test_Text_analyser {
+
+	static StanfordCoreNLP pipeline;
+
+	public void init() {
+		Properties props = new Properties();
+		props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
+		pipeline = new StanfordCoreNLP(props);
+	}
+
+	public String findSentiment(String tweet) {
+
+		int mainSentiment = 0;
+		if (tweet != null && tweet.length() > 0) {
+			int longest = 0;
+			Annotation annotation = pipeline.process(tweet);
+			for (CoreMap sentence : annotation
+					.get(CoreAnnotations.SentencesAnnotation.class)) {
+				Tree tree = sentence
+						.get(SentimentAnnotatedTree.class);
+				int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
+				SimpleMatrix sentiment_new = RNNCoreAnnotations.getPredictions(tree);
+				String partText = sentence.toString();
+				if (partText.length() > longest) {
+					mainSentiment = sentiment;
+					longest = partText.length();
+				}
+			}
+		}
+
+		if(mainSentiment > 2)
+			return "positive";
+		else
+			return "negative";
+	}
+
+}
